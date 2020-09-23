c335bcc66a669989daf946d0cdf8da8057283d88
Leonid Vakar <leonid.vakar@gmail.com>
2020-09-24 00:08:31 +0300

    chore (server): split bundle to chunks

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,60 @@
-let path = require('path');
+let path = require("path");
 
 module.exports = {
-    entry: './src/main/js/app.js',
-    devtool: 'sourcemaps',
-    cache: true,
-    mode: 'development',
-    output: {
-        path: __dirname,
-        filename: './src/main/resources/static/built/bundle.js',
-        publicPath: '/built/'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules)/,
-                use: [{
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ["@babel/preset-env", "@babel/preset-react"]
-                    }
-                }]
+  entry: "./src/main/js/app.js",
+  // devtool: "sourcemaps",
+  cache: true,
+  mode: "development",
+  output: {
+    path: __dirname,
+    filename: "./src/main/resources/static/built/bundle.js",
+    publicPath: "/built/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
             },
-            {
-                test: /\.css$/,
-                loader: 'style-loader!css-loader'
-            }
-        ]
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+      minSize: 0,
+      maxInitialRequests: 10,
+      maxAsyncRequests: 10,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/](react|react-dom|bootstrap)[\\/]/,
+          name(module, chunks, cacheGroupKey) {
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )[1];
+            return `${cacheGroupKey}.${packageName.replace("@", "")}`;
+          },
+        },
+        common: {
+          minChunks: 2,
+          priority: -10,
+        },
+      },
     },
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
-    },
+    runtimeChunk: "single",
+  },
 };
